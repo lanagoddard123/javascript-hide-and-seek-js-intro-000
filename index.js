c56cbe82fad361ca1609ce2cfb3e19d126a35654
lanasgoddard <lanasgoddard@gmail.com>
2019-02-04 13:46:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function increaseRankBy(n){
     rankedLists[i].textContent = (parseInt(rankedLists[i].textContent) + n).toString();
   }
 }
+
+function deepestChild() {
+  let parent = document.getElementById('grand-node');
+  // const children = Array.from(parent.querySelectorAll('div'));
+  // return children;
+  // let children = Array.from(parent.children);
+  let child = parent.children[0];
+  while (child) {
+    parent = child;
+    child = parent.children[0];
+  }
+  // return children.find(div => div.textContent === "boo!");
+  return parent;
+};
