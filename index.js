008a473e5bb09401cac2bc1fe84cdb668f744487
lanasgoddard <lanasgoddard@gmail.com>
2019-02-04 13:44:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function nestedTarget(target){
   .getElementById('nested')
   .getElementsByClassName('target')[0]
 }
+function increaseRankBy(n){
+  const rankedLists = document.getElementsByClassName('ranked-list');
+  for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].textContent = (parseInt(rankedLists[i].textContent) + n).toString();
+  }
+}
