d55fa368191aa003491f8e094850491dcc5b39f3
lanasgoddard <lanasgoddard@gmail.com>
2019-02-04 13:41:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function getFirstSelector(selector){
   return document.querySelector(selector)
 }
+
+function nestedTarget(target){
+  return document
+  .getElementById('nested')
+  .getElementsByClassName('target')[0]
+}
